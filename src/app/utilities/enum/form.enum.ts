f198d650e19fb6e0632c3434aa33d84268a89a10
viacharles <viacharles@gmail.com>
2024-04-19 15:27:42 +0800

    新增 form 相關 enum

--- src/app/utilities/enum/form.enum.ts
+++ src/app/utilities/enum/form.enum.ts
@@ -2,17 +2,29 @@
  * 有用於i18n
 */
 export enum EErrorMessage {
+  /** 有用在 i18n */
   REQUIRED = 'validator.required',
+  /** 有用在 i18n */
   MAX_LENGTH = 'validator.max-length',
+  /** 有用在 i18n */
   MIN_LENGTH = 'validator.min-length',
+  /** 有用在 i18n */
   MAX_MIN_LENGTH = 'validator.max-min-length',
+  /** 有用在 i18n */
   MAX_ITEMS = 'validator.max-items',
+  /** 有用在 i18n */
   MIN_ITEMS = 'validator.min-items',
+  /** 有用在 i18n */
   MAX_MIN_ITEMS = 'validator.max-min-items',
+  /** 有用在 i18n */
   NUMBER_ONLY = 'validator.number-only',
+  /** 有用在 i18n */
   CN_ONLY = 'validator.cn-only',
+  /** 有用在 i18n */
   EN_NUMBER_ONLY = 'validator.en-number-only',
+  /** 有用在 i18n */
   FORMAT_ERROR = 'validator.format',
+  /** 有用在 i18n */
   EMAIL_ERROR = 'validator.email'
 }
 
@@ -34,6 +46,23 @@ export enum EFieldMemoType {
   Default = 'default',
   /** 範圍:兩欄 [min]-[max] */
   Range = 'range',
+}
+
+/** 輸入欄型態 */
+export enum EInputType {
+  Text = 'text',
+  Number = 'number',
+  Email = 'email',
+  Password = 'password',
+  Tel = 'tel',
+  Url = 'url',
+  Search = 'search',
+  Date = 'date',
+  Time = 'time',
+  DateTimeLocal = 'datetime-local',
+  Month = 'month',
+  Week = 'week',
+  Color = 'color',
 }
 
 
