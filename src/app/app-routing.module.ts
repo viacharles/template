fa74f36f20916f9d5638b5b59170868ba17013aa
viacharles <viacharles@gmail.com>
2024-06-04 16:06:32 +0800

    app / app-routing: 修正 layout 排版層級

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,34 +8,27 @@ import {AuthGuard, DefaultGuard} from './core/auth.guard';
 import {AuthenticationService} from './core/services/authentication.service';
 
 // components
-import { LayoutBaseComponent } from './modules/layout/layout-base/layout-base.component';
 import { PageNotFoundComponent } from './modules/layout/page-not-found/page-not-found.component';
 
 export const baseRoutes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {
-    path: '',
-    component: LayoutBaseComponent,
-    children: [
-      {
-        path: 'home',
-        canActivateChild: [DefaultGuard],
-        loadChildren: () =>
-          import('./modules/home/home.module').then(m => m.HomeModule),
-      },
-      {
-        path: `${EModule.Form}`,
-        canActivateChild: [AuthGuard],
-        loadChildren: () =>
-        import('./modules/form/form-elements.module').then(m => m.FormElementsModule),
-      },
-      {
-        path: `${EModule.Table}`,
-        canActivateChild: [AuthGuard],
-        loadChildren: () =>
-        import('./modules/table/table-page.module').then(m => m.TablePageModule),
-      },
-    ],
+    path: 'home',
+    canActivateChild: [DefaultGuard],
+    loadChildren: () =>
+      import('./modules/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: `${EModule.Form}`,
+    canActivateChild: [AuthGuard],
+    loadChildren: () =>
+    import('./modules/form/form-elements.module').then(m => m.FormElementsModule),
+  },
+  {
+    path: `${EModule.Table}`,
+    canActivateChild: [AuthGuard],
+    loadChildren: () =>
+    import('./modules/table/table-page.module').then(m => m.TablePageModule),
   },
   {path: '**', component: PageNotFoundComponent},
 ];
