1acd95bdc31d382ac8b18e43323e1e1a93f3eb01
viacharles <viacharles@gmail.com>
2024-06-04 11:42:11 +0800

    go-top-button 修改樣式

--- src/app/shared/components/buttons/go-top-button/go-top-button.component.ts
+++ src/app/shared/components/buttons/go-top-button/go-top-button.component.ts
@@ -1,12 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-go-top-button',
   templateUrl: './go-top-button.component.html',
   styleUrls: ['./go-top-button.component.scss'],
 })
-export class GoTopButtonComponent implements OnInit {
-  constructor() {}
+export class GoTopButtonComponent {
+  constructor(
+    private renderer: Renderer2
+  ) {}
 
-  ngOnInit(): void {}
+  public click(button: HTMLButtonElement): void {
+    this.renderer.addClass(button, 'clicked');
+    setTimeout(() => {
+      this.renderer.removeClass(button, 'clicked');
+    }, 300);
+  }
 }
