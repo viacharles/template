f1ffe4170f7b6282e2068f3241ab74519b64875f
viacharles <viacharles@gmail.com>
2024-06-13 09:58:50 +0800

    dialof-container 修改關閉樣式

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,19 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {IconDropdownComponent} from './components/dropdown/icon-dropdown/icon-dropdown.component';
-import {NoDataSectionComponent} from './components/sections/no-data-section/no-data-section.component';
-import {TranslateModule} from '@ngx-translate/core';
-import {AccordionComponent} from './components/accordion/accordion.component';
-import {PageTabSetComponent} from './components/layout/page-tabset/page-tab-set.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {SimpleCardComponent} from './components/cards/simple-card/simple-card.component';
-import {TooltipComponent} from './components/tooltip/tooltip/tooltip.component';
-import {PipeModule} from './pipes/pipe.module';
-import {LoadingOverlaySectionComponent} from './components/sections/loading-overlay-section/loading-overlay-section.component';
-import {BreadCrumbsComponent} from './components/bread-crumbs/bread-crumbs.component';
-import {TabsComponent} from './components/tab/tabs.component';
-import {SideTabPopupDialogComponent} from './components/overlay/side-tab-popup-dialog/side-tab-popup-dialog.component';
-import {AccordionListCardComponent} from './components/accordion/accordion-list-card/accordion-list-card.component';
-import { SortThComponent } from './components/icon/sort-th/sort-th.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconDropdownComponent } from './components/dropdown/icon-dropdown/icon-dropdown.component';
+import { NoDataSectionComponent } from './components/sections/no-data-section/no-data-section.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { AccordionComponent } from './components/accordion/accordion.component';
+import { PageTabSetComponent } from './components/layout/page-tabset/page-tab-set.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SimpleCardComponent } from './components/cards/simple-card/simple-card.component';
+import { TooltipComponent } from './components/tooltip/tooltip/tooltip.component';
+import { PipeModule } from './pipes/pipe.module';
+import { LoadingOverlaySectionComponent } from './components/sections/loading-overlay-section/loading-overlay-section.component';
+import { BreadCrumbsComponent } from './components/bread-crumbs/bread-crumbs.component';
+import { TabsComponent } from './components/tab/tabs.component';
+import { SideTabPopupDialogComponent } from './components/overlay/side-tab-popup-dialog/side-tab-popup-dialog.component';
+import { AccordionListCardComponent } from './components/accordion/accordion-list-card/accordion-list-card.component';
 import { ButtonsModule } from './components/buttons/buttons.module';
 
 @NgModule({
@@ -30,7 +29,6 @@ import { ButtonsModule } from './components/buttons/buttons.module';
     TabsComponent,
     SideTabPopupDialogComponent,
     AccordionListCardComponent,
-    SortThComponent,
   ],
   exports: [
     IconDropdownComponent,
@@ -44,7 +42,6 @@ import { ButtonsModule } from './components/buttons/buttons.module';
     TabsComponent,
     SideTabPopupDialogComponent,
     AccordionListCardComponent,
-    SortThComponent,
     PipeModule,
     ButtonsModule
   ],
